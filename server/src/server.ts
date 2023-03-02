f918c6a9ba2a7d0dc1e147a2bc230eab3acd6eaf
Onroe <leninkaha@gmail.com>
2023-03-02 18:14:17 +0300

    part-2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,19 @@
 import  "dotenv/config";
+import env from  "./util/validateEnv";
 import mongoose from "mongoose";
 
 import express from "express";
 const app = express();
 
 
-app.get("/",(req,res) => {
-res.send("Hello, World");
-});
+const port  = env.PORT;
+mongoose.connect(env.MONGO_DB_URI)
+ .then (() => {
+    console.log("Mongoose connected");
+    app.listen(port, () => {
+        console.log("Server running on port: " + port);
+ });
 
-const port  = process.env.PORT;
-console.log("Server running on port: " + port);
+})
+
+.catch(console.error);
