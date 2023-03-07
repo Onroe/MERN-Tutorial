f80f4331bff7d56eb3b30363a56691139e3b8516
Onroe <leninkaha@gmail.com>
2023-03-07 17:04:25 +0300

    part-8

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
-import './App.css';
-import { Note } from './models/note';
+import './styles/global.css';
+import { Note as NoteModel } from './models/note';
+import Note from './components/Note';
+
+
 function App() {
 
 
- const [notes, setNotes] = useState<Note[]>([]);
+ const [notes, setNotes] = useState<NoteModel[]>([]);
 
  useEffect (()  => {
   async function loadNotes() {
@@ -24,7 +27,9 @@ function App() {
 
   return (
     <div className="App">
-      {JSON.stringify(notes)}
+     {notes.map(note => (
+      <Note note={note} key={note._id} />
+     ))}
     </div>
   );
 }
