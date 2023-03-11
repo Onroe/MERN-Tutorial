fc4d5b0fcae5d25b31f98be34aadda1bbd1bef80
Onroe <leninkaha@gmail.com>
2023-03-11 12:25:43 +0300

    part-12

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,15 +1,34 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
 import noteRoutes from "./routes/notes";
+import userRoutes from "./routes/users";
 import morgan from "morgan";
 import createHttpError, { isHttpError } from "http-errors";
+import session from "express-session";
+import env from "./util/validateEnv";
+import MongoStore from "connect-mongo";
+
 
 const app = express();
 
 app.use(morgan("dev"));
 
-app.use(express.json());
+app.use(express.json());1
 
+app.use(session({
+    secret: env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+    },
+    rolling: true,
+    store: MongoStore.create({
+        mongoUrl: env.MONGO_DB_URI
+    }),
+}));
+
+app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
 
 app.use((req,res, next) => {
